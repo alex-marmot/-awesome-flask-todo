473ba6c17afb072d51c7a75a351a47f857aab562
alex-marmot <alex.marmot@gmail.com>
2015-06-04 00:29:06 +0800

    project scoffold

---
+++ manage.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from flask.ext.script import Manager
+from flask.ext.script import Server
+from app import app
+
+manager = Manager(app)
+manager.add_command("runserver",
+                    Server(host='0.0.0.0', port=5000, use_debugger=True))
+
+if __name__ == '__main__':
+    manager.run()
